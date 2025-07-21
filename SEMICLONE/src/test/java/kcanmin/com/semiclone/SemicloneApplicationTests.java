b8f412cd228d3c04fd42ae2ca79ba696b94a3ce1
DaHnDell <khanman1208@gmail.com>
2025-07-21 12:58:07 +0900

    Main Application & Test Files

---
+++ SEMICLONE/src/test/java/kcanmin/com/semiclone/SemicloneApplicationTests.java
@@ -0,0 +1,13 @@
+package kcanmin.com.semiclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemicloneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
